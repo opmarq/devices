cf7924b6f616d8247aff0eaa82d1b89c86144ec3
opmarq <opmarq@gmail.com>
2021-02-19 12:45:59 +0100

    First setup & cleanup

---
+++ frontend/src/api/api.test.ts
@@ -0,0 +1,24 @@
+import axios from "./api";
+import { mocked } from "ts-jest/utils";
+
+import { fetchDevices } from "./api";
+
+import { mock_devices } from "./mocks/data";
+
+jest.mock("axios");
+
+const mockedAxios = mocked(axios, true);
+
+describe("fetchDevices", () => {
+  it("fetches successfuly devices from the api", async () => {
+    mockedAxios.get.mockImplementationOnce(() => Promise.resolve(mock_devices));
+    await expect(fetchDevices(10)).resolves.toEqual(mock_devices);
+  });
+  it("fetches erroneously devices from the api", async () => {
+    const errorMessage = "Network Error";
+    mockedAxios.get.mockImplementationOnce(() =>
+      Promise.reject(new Error(errorMessage))
+    );
+    await expect(fetchDevices(10)).rejects.toThrow(errorMessage);
+  });
+});
---
+++ frontend/src/api/api.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const devEndPont = "http://localhost:8010";
+
+const axiosInstance = axios.create({
+  baseURL: devEndPont,
+});
+
+export const fetchDevices = (n: number) => {
+  return axiosInstance.get("/devices", {
+    params: {
+      n,
+    },
+  });
+};
+
+export default axiosInstance;
