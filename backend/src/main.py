fd5da402d20462a04a4e414795b2481bbd01af80
opmarq <opmarq@gmail.com>
2021-02-19 19:03:05 +0100

    Cors

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,6 +2,14 @@
 from fastapi import FastAPI
 from typing import List
 from .model import Device, generate_device
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:3001",
+    "http://localhost:3000",
+    "http://localhost:8080",
+    "http://localhost:8000",
+]
 
 app = FastAPI(
     title=f"GUI Test App",
@@ -9,6 +17,13 @@ app = FastAPI(
     debug=True,
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/devices", response_model=List[Device])
