269313ed6ad30960da8557f3feb2e638174b91d6
opmarq <opmarq@gmail.com>
2021-02-22 12:45:06 +0100

    Make logo clickable

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -3,13 +3,14 @@ import styled from "styled-components";
 
 import svglogo from "../statics/svg/logo-upciti.svg";
 
-const StyledLogo = styled.div`
+const StyledLogo = styled.a`
+  display: block;
   padding: 30px 20px;
 `;
 
 const Logo = () => {
   return (
-    <StyledLogo>
+    <StyledLogo href="/">
       <img width="100%" src={svglogo} />
     </StyledLogo>
   );
